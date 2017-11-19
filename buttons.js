32c86a3dbc29605cff45642e1e74d894bc64fc99
Julia Luo <julia.luo98@gmail.com>
2017-11-19 13:54:26 -0500

    Update buttons.js

--- buttons.js
+++ buttons.js
@@ -40,5 +40,5 @@ function changeMonth(evt, tabName){
 }
 
 function changeMap(time, day, month){
-    document.getElementById("map").innerHTML = '<img class="photo" src="' + time + '_' + day + '_' + month +'.png" width=100% >';
+    document.getElementById("map").innerHTML = '<img class=\"photo\" src=\"' + time + '_' + day + '_' + month + '.png" width=1000px >';
 }
